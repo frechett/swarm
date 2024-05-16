b80b60f9b24bf261a11fb2e7470fcadba35e0326
Kevin Frechette <k.frechette@isti.com>
2024-05-16 13:28:38 -0400

    Update POM_VERSION to match pom

--- src/main/java/gov/usgs/volcanoes/swarm/Version.java
+++ src/main/java/gov/usgs/volcanoes/swarm/Version.java
@@ -2,9 +2,9 @@ package gov.usgs.volcanoes.swarm;
 
 public final class Version {
 
-    public static final String BUILD_TIME="2020-06-09T17:56:56Z";
+    public static final String BUILD_TIME="2024-05-16T17:26:48Z";
 
-    public static final String POM_VERSION="3.2.0";
+    public static final String POM_VERSION="3.2.1";
 
 	public static final String VERSION_STRING = "Version: " + POM_VERSION + " Built: " + BUILD_TIME;
 }
